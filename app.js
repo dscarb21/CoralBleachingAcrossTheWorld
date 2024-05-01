74d1dc1c14b5bc3840d81a5b46871f0be1a27835
David Scarborough <dscarb21@gmail.com>
2024-05-01 15:52:19 +0200

    Removed public dir so github pages would work

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
